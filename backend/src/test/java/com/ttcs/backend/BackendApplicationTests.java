bd67a158106f0d51b6f1bcc666c0aaa785afe045
Phan Manh Cuong <cuongpm.b23cn115@stu.ptit.edu.vn>
2026-03-16 21:26:15 +0700

    init project (spring boot backend + react frontend)

---
+++ backend/src/test/java/com/ttcs/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttcs.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
